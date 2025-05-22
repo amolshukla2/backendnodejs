28ca0d98719409d20a42370b64784fe379a6f111
dell <226301025@gkv.ac.in>
2025-05-22 09:50:36 -0700

    as

--- cap/rt.js
+++ cap/rt.js
@@ -1,1 +1,12 @@
-ddsg
+create folder -mkdir
+crate File -echo > rt.js
+ push Step   
+ check status  using git status
+
+ then add all using add.
+ then commit using commit -m "Initial commit"
+ then push using push origin main
+ to ccreate branch
+ git branch -b branch_name
+ git checkout branch_name
+ git pull origin branch_name
